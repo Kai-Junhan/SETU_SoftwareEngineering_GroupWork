880a71d75fdac54b0b3819fd03ed93a5843bc74a
KaiJunhan <3584801405@qq.com>
2025-12-03 17:50:58 +0800

    delet garbage

--- SoftwareEngineering_GroupWork/LibraryManagementSystem_v2.0/LibarayManagementSystem/src/main/java/com/library/model/User.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.library.model;
-
-//user entity class
-public class User {
-    private String userName;
-    private String userId;
-    private String userPassword;
-}
--- SoftwareEngineering_GroupWork/LibraryManagementSystem_v3.0/LibraryManagementSystem/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo 正在运行图书管理系统...
-echo.
-cd /d "%~dp0"
-java -cp "out/production/NUIST-SETU-HW;src/main/resources" com.library.Main
-pause
